ecbc5a3615474a22f9d2751cfe099a00c2cca9fe
mounikaselvaraj <mounikaraj04@gmail.com>
2026-04-04 16:20:12 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const serverless = require("serverless-http");
+
 
 const app = express();
 app.use(cors({

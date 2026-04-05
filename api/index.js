e92b11c5c8746f05248e80f772a81ff5319e4d32
mounikaselvaraj <mounikaraj04@gmail.com>
2026-04-05 07:57:23 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -159,8 +159,10 @@ app.get("/api/dashboard", verifyToken, (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(` Server running on port ${PORT}`);
-});
+// app.listen(PORT, '0.0.0.0', () => {
+//   console.log(` Server running on port ${PORT}`);
+// });
+
+module.exports = app;
